640cd49196615713ba95b1c1b942efffde49a36e
AuroraHan <1224195854@qq.com>
2022-10-01 00:00:03 +0800

    初始化项目

---
+++ src/pages/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <view class="content">
+    <image class="logo" src="../../static/logo.png"></image>
+    <view>
+      <text class="title">{{ title }} 你好啊十三点</text>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { onShow, onHide, onLoad } from '@dcloudio/uni-app'
+
+const title = ref('')
+onLoad((options) => {
+  console.log('page query', options)
+})
+
+// 页面切换到前台时
+onShow(() => {
+  title.value = 'hello uni-app-vue3 setup'
+})
+
+// 页面切换到后台时
+onHide(() => {
+  title.value = 'see you'
+})
+</script>
+
+<style lang="scss">
+.content {
+  text-align: center;
+  height: $test-size;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
