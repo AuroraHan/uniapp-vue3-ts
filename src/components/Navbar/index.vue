ddff78ec3dee45f13f6761c4a3227dca8b2e2e20
AuroraHan <1224195854@qq.com>
2022-10-01 23:01:49 +0800

    文件处理

--- src/components/Navbar/index.vue
+++ src/components/Navbar/index.vue
@@ -1,35 +1,10 @@
 <template>
-  <view class="content">
-    <view>这是子组件</view>
-    <view>传递过来的数据:{{ props.foo }}</view>
-    <view @click="emitData">点击传递数据</view>
-  </view>
+  <view class="content"> </view>
 </template>
 
 <script setup lang="ts">
-import { defineProps, onMounted, defineEmits } from 'vue'
-import { onLoad } from '@dcloudio/uni-app'
-
-const props = defineProps({
-  foo: {
-    type: String,
-    default: '',
-  },
-})
-
-const emit = defineEmits(['change'])
-
-function emitData() {
-  emit('change', '向父组件传递的数据')
-}
-
-onMounted(() => {
-  console.log(props.foo, '00')
-})
-
-onLoad((options) => {
-  console.log('page query', options)
-})
+// import { defineProps, onMounted, defineEmits } from 'vue'
+// import { onLoad } from '@dcloudio/uni-app'
 </script>
 
 <style lang="scss">
