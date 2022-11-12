cc7a547c7fc916152d76816d3299af7b29391106
AuroraHan <1224195854@qq.com>
2022-11-13 00:38:46 +0800

    个人中心头部编写

--- src/pages/center/main/index.vue
+++ src/pages/center/main/index.vue
@@ -1,7 +1,19 @@
 <template>
   <Navbar title="个人中心" :bg-color="bgColor"></Navbar>
   <view :class="[ns.b()]">
-    <view> dfjdk </view>
+    <view :class="[ns.b('user')]">
+      <image :class="[ns.be('user', 'img')]" src="@/static/logo.png"></image>
+      <!-- 用户信息 -->
+      <view :class="[ns.be('user', 'info')]">
+        <view :class="ns.bem('user', 'info', 'left')">
+          <view>乘风破浪</view>
+          <view>乘风破浪</view>
+          <view>乘风破浪</view>
+        </view>
+        <!-- 二维码 -->
+        <image :class="[ns.bem('user', 'info', 'dimensional')]" src="@/static/logo.png"></image>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -10,13 +22,40 @@ import { ref } from 'vue'
 import Navbar from '@/components/Navbar/index.vue'
 import { useNamespace } from '@/hooks/use-namespace'
 
-const ns = useNamespace('my')
+const ns = useNamespace('center')
 console.log(ns.b())
 const bgColor = ref('#f3d19e')
 </script>
 
 <style lang="scss">
-@include block(my) {
-  color: red;
+@include block(center) {
+  padding: 10px;
+  background-color: $border-color-dark;
+  height: 100vh;
+}
+
+@include block(center-user) {
+  padding: 5px;
+  box-sizing: border-box;
+  display: flex;
+  background-color: $color-danger;
+
+  @include element(img) {
+    height: 180rpx;
+    width: 180rpx;
+    border-radius: 50%;
+  }
+
+  @include element(info) {
+    display: flex;
+    justify-content: space-between;
+    flex: 1;
+    padding: 0 5px;
+
+    @include modifier(dimensional) {
+      height: 180rpx;
+      width: 180rpx;
+    }
+  }
 }
 </style>
