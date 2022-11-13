bb8d8756dd138a87b5fd3b3fc2814e51540f3cc0
AuroraHan <1224195854@qq.com>
2022-11-13 17:04:31 +0800

    完成个人中心

--- src/pages/center/main/index.vue
+++ src/pages/center/main/index.vue
@@ -6,9 +6,9 @@
       <!-- 用户信息 -->
       <view :class="[ns.be('user', 'info')]">
         <view :class="ns.bem('user', 'info', 'left')">
-          <view>乘风破浪</view>
-          <view>乘风破浪</view>
-          <view>乘风破浪</view>
+          <view :class="[ns.b('name')]">乘风破浪 <text :class="[ns.be('name', 'certification')]">已认证</text></view>
+          <view :class="[ns.b('phone')]"><text :class="[ns.be('phone', 'text')]">绑定</text>13452346543</view>
+          <view :class="[ns.b('vip')]">VIP客户</view>
         </view>
         <!-- 二维码 -->
         <image :class="[ns.bem('user', 'info', 'dimensional')]" src="@/static/logo.png"></image>
@@ -23,22 +23,21 @@ import Navbar from '@/components/Navbar/index.vue'
 import { useNamespace } from '@/hooks/use-namespace'
 
 const ns = useNamespace('center')
-console.log(ns.b())
 const bgColor = ref('#f3d19e')
 </script>
 
 <style lang="scss">
 @include block(center) {
   padding: 10px;
-  background-color: $border-color-dark;
   height: 100vh;
+  background-color: $color-theme-tint;
 }
 
 @include block(center-user) {
   padding: 5px;
   box-sizing: border-box;
   display: flex;
-  background-color: $color-danger;
+  background-color: $color-theme-white;
 
   @include element(img) {
     height: 180rpx;
@@ -52,10 +51,37 @@ const bgColor = ref('#f3d19e')
     flex: 1;
     padding: 0 5px;
 
+    @include modifier(left) {
+      padding: 5px;
+    }
+
     @include modifier(dimensional) {
       height: 180rpx;
       width: 180rpx;
     }
   }
+}
+
+@include block(center-name) {
+  margin-bottom: 6px;
+  @include element(certification) {
+    background: $color-primary;
+    color: $font-color-white;
+    font-size: 12px;
+  }
+}
+
+@include block(center-phone) {
+  font-size: 14px;
+  margin-bottom: 6px;
+  @include element(text) {
+    padding: 0 3px;
+    background-color: bisque;
+  }
+}
+
+@include block(center-vip) {
+  font-weight: 600;
+  color: bisque;
 }
 </style>
